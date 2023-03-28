2638a54233b43b5bb692a359a2bbc3694cb0e252
Tarik Doganay <99910240+GoodGuyForReal@users.noreply.github.com>
2023-03-28 17:04:05 +0200

    hero section design updated bg color changed and bg-svg added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Home from "./components/pages/home/Home";
 import Navbar from "./elements/navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Home />
     </div>
   );
 }

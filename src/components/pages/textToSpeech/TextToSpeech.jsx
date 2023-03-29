8b8bff4eafa37398d32f626af8b768f94e4ca1ec
Tarik Doganay <99910240+GoodGuyForReal@users.noreply.github.com>
2023-03-30 00:15:52 +0200

    Text to Audio page design update

--- src/components/pages/textToSpeech/TextToSpeech.jsx
+++ src/components/pages/textToSpeech/TextToSpeech.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react'
+import BgBlurSvg from '../../../assets/BgBlurSvg'
+import BgBlurSvgTop from '../../../assets/BgBlurSvgTop'
 import { TextToAudio } from '../../../repository/TextToSpeech'
 
 const TextToSpeech = () => {
@@ -12,28 +14,29 @@ const TextToSpeech = () => {
     console.log(audio);
 
     return (
-        <div className='bg-slate-900 h-screen flex flex-col justify-center'>
-            <form onSubmit={textToAudioHandler}>
-                <div className="col-span-full">
-                    <label htmlFor="about" className="block text-sm font-medium leading-6 text-white">
-                        Write a Text that you want to convert audio
-                    </label>
-                    <div className="mt-2">
+        <div className='TextToSpeech relative isolate min-h-screen overflow-y-hidden bg-slate-900 flex flex-col justify-center items-center gap-10'>
+            <div className='ArticleByURLHeader flex flex-col gap-10 items-center max-w-3xl w-full'>
+                <h1 className="block text-5xl font-bold text-white">
+                    Text to Audio
+                </h1>
+
+                <form onSubmit={textToAudioHandler} className=' w-full'>
+                    <div className="flex flex-col gap-5 w-full">
                         <textarea
-                            id="about"
-                            name="about"
-                            rows={3}
-                            className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:py-1.5 sm:text-sm sm:leading-6"
+                            value={text}
+                            className="block w-full rounded-md border-0 text-gray-900 h-[20vh] p-3 resize-none"
+                            placeholder='Write smth'
                             onChange={(e) => setText(e.target.value)}
                         />
+                        <input type="submit"
+                            value={'Convert'}
+                            className="rounded-md cursor-pointer bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 duration-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+                        />
                     </div>
-                    <p className="mt-3 text-sm leading-6 text-gray-600">Write a few sentences about yourself.</p>
-                </div>
-                <input type="submit"
-                    value={'Convert'}
-                    className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 duration-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
-                />
-            </form>
+                </form>
+
+            </div>
+
 
             <div>
                 {audio && <audio controls src={audio} />}
@@ -41,7 +44,8 @@ const TextToSpeech = () => {
             </div>
 
 
-
+            <BgBlurSvgTop />
+            <BgBlurSvg />
         </div>
     )
 }
